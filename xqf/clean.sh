4daf14906090419a22797c378e20fb9fcfd3f8ac
Alex Burger <alex_b@users.sourceforge.net>
2001-09-06 15:27:49 +0000

    Missing files added
    git-svn-id: http://svn.code.sf.net/p/xqf/code/trunk@59 d2ac09be-c843-0410-8b1f-f8a84130e0ec

---
+++ xqf/clean.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+rm -f \
+Makefile \
+config.status \
+config.cache \
+config.log \
+configure \
+Makefile.in \
+aclocal.m4 \
+config.guess \
+config.sub \
+install-sh \
+mkinstalldirs \
+missing \
+*/Makefile \
+src/Makefile.in \
+ltconfig \
+ltmain.sh \
+libtool \
+config.h \
+intl/libintl.h \
+src/gnuconfig.h \
+src/gnuconfig.h.in \
+src/xpm/Makefile.in \
+src/xpm/Makefile \
+announce/Makefile.in
