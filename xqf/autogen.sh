5463ee84ca5eeb38cd66f9230d8acb086f1d86d5
Ludwig Nussel <ludwig.nussel@suse.de>
2002-09-16 19:33:09 +0000

    changed some macros to make cvs work with older autoconf/automake error checking in autogen.sh
    git-svn-id: http://svn.code.sf.net/p/xqf/code/trunk@186 d2ac09be-c843-0410-8b1f-f8a84130e0ec

--- xqf/autogen.sh
+++ xqf/autogen.sh
@@ -1,5 +1,12 @@
-libtoolize --force
-aclocal
-autoheader
-automake -a
-autoconf
+#!/bin/bash
+die()
+{
+	echo "$1 failed, are you sure you have the right version of $2 installed?"
+	exit 1
+}
+
+libtoolize --force || die libtoolize libtool
+aclocal || die aclocal automake
+autoheader || die autoheader autoconf
+automake -a || die automake automake
+autoconf || die autoconf autoconf
