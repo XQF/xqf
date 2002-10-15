c73a2b6f53c272dba2bb1f803eef5520c046d2fa
Bill Adams <bill@evilbill.org>
2002-10-15 20:29:09 +0000

    set WANT_AUTOCONF_2_5 in autogen.sh
    git-svn-id: svn+ssh://svn.code.sf.net/p/xqf/code/trunk@251 d2ac09be-c843-0410-8b1f-f8a84130e0ec

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,11 @@ die()
 	exit 1
 }
 
+#In case people have both autoconf 2.5 and 2.1 installed,
+#  we will try to select 2.5 -baa
+export WANT_AUTOCONF_2_5=1
+
+
 libtoolize --force || die libtoolize libtool
 aclocal || die aclocal automake
 autoheader || die autoheader autoconf
