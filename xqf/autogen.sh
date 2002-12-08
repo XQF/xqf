8d4a36dac4188994502de6492c6b0dc2cb23e82b
Ludwig Nussel <ludwig.nussel@suse.de>
2002-12-08 18:05:29 +0000

    update configure.in and autogen.sh to match newer automake/autoconf
    git-svn-id: http://svn.code.sf.net/p/xqf/code/trunk@404 d2ac09be-c843-0410-8b1f-f8a84130e0ec

--- xqf/autogen.sh
+++ xqf/autogen.sh
@@ -10,8 +10,22 @@ die()
 export WANT_AUTOCONF_2_5=1
 
 
+# workaround for libtoolize putting files in ..
+if [ ! -e install-sh ]; then
+	removeinstallsh=1
+	touch install-sh
+fi
+
+echo "running libtoolize ..."
 libtoolize --force || die libtoolize libtool
+
+[ -n "$removeinstallsh" ] && rm install-sh
+
+echo "running aclocal ..."
 aclocal || die aclocal automake
+echo "running autoheader ..."
 autoheader || die autoheader autoconf
+echo "running automake ..."
 automake -a || die automake automake
+echo "running autoconf ..."
 autoconf || die autoconf autoconf
