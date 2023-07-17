ae11752eda6548683b72fbe02f77199da9fc821b
Zack Middleton <zack@cloemail.com>
2023-07-17 15:48:51 -0500

    Replace GtkPreview

--- src/skin.c
+++ src/skin.c
@@ -20,6 +20,7 @@
 #include <string.h>     /* strlen, strcpy, strncpy, strcmp, strncmp, memset */
 #include <sys/stat.h>   /* stat */
 #include <unistd.h>     /* stat */
+#include <math.h>       /* pow */
 
 #include <gtk/gtk.h>
 
@@ -47,6 +48,9 @@ static guchar quake2_pallete[768] = {
 
 static GdkColor pcolors[14];
 static int pcolors_allocated = FALSE;
+
+static guchar gamma_lookup[256];
+static double gamma_lookup_value = 1.0;
 
 
 static char *dir_qw_skin_filter (const char *dir, const char *str) {
@@ -154,13 +158,75 @@ GList *get_q2_skin_list (const char *dir) {
 }
 
 
-void draw_qw_skin (GtkWidget *preview, guchar *data, int top, int bottom) {
+// Copied from GTK2 gtk_fill_lookup_array()
+static void pixbuf_draw_set_gamma (double gamma) {
+	double one_over_gamma;
+	double ind;
+	int val;
+	int i;
+
+	if (gamma_lookup_value == gamma) {
+		return;
+	}
+
+	one_over_gamma = 1.0 / gamma;
+
+	for (i = 0; i < 256; i++) {
+		ind = (double) i / 255.0;
+		val = (int) (255 * pow (ind, one_over_gamma));
+		gamma_lookup[i] = val;
+	}
+
+	gamma_lookup_value = gamma;
+}
+
+static void pixbuf_draw_row (GdkPixbuf *pixbuf, guchar *data, int x, int y, int width) {
+	guchar *pixels = gdk_pixbuf_get_pixels (pixbuf);
+	int n_channels = gdk_pixbuf_get_n_channels (pixbuf);
+	int rowstride  = gdk_pixbuf_get_rowstride (pixbuf);
+	int pb_width  = gdk_pixbuf_get_width (pixbuf);
+	int pb_height  = gdk_pixbuf_get_height (pixbuf);
+
+	if (x < 0 || y < 0 || width < 0 || x+width > pb_width || y+1 > pb_height) {
+		g_warning ("pixbuf_draw_row: invalid draw bounds (x: %d, y: %d, w: %d) for pixbuf (w: %d, h: %d)", x, y, width, pb_width, pb_height);
+		return;
+	}
+
+	 guchar *dst = pixels + y * rowstride + x * n_channels;
+
+	if (gamma_lookup_value == 1.0) {
+		memcpy (dst, data, width * n_channels);
+	} else {
+		guchar *src = data;
+		int i;
+
+		for (i = 0; i < width; i++) {
+			dst[0] = gamma_lookup[ src[0] ];
+			dst[1] = gamma_lookup[ src[1] ];
+			dst[2] = gamma_lookup[ src[2] ];
+
+			if (n_channels == 4) {
+				dst[3] = src[3];
+			}
+
+			src += n_channels;
+			dst += n_channels;
+		}
+	}
+}
+
+
+void draw_qw_skin (GtkWidget *image, guchar *data, int top, int bottom) {
+	GdkPixbuf *pixbuf;
 	guchar buf[320*3];
 	guchar colormap[256];
 	guchar *ptr;
 	int i, j, k;
 	int fixed_top_color = fix_qw_player_color (top);
 	int fixed_bottom_color = fix_qw_player_color (bottom);
+
+	pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, 320, 200);
+	pixbuf_draw_set_gamma (1.5);
 
 	if (data) {
 		for (i = 0; i < 256; i++)
@@ -186,7 +252,7 @@ void draw_qw_skin (GtkWidget *preview, guchar *data, int top, int bottom) {
 				*ptr++ = quake_pallete[colormap[*data] * 3 + 2];
 				data++;
 			}
-			gtk_preview_draw_row (GTK_PREVIEW (preview), buf, 0, i, 320);
+			pixbuf_draw_row (pixbuf, buf, 0, i, 320);
 		}
 	}
 	else {
@@ -197,20 +263,26 @@ void draw_qw_skin (GtkWidget *preview, guchar *data, int top, int bottom) {
 				i = (NOSKIN_WCOLOR + NOSKIN_BCOLOR) - i;
 			}
 			for (i = 0; i < 40; i++) {
-				gtk_preview_draw_row (GTK_PREVIEW (preview), buf, 0, j+i, 320);
+				pixbuf_draw_row (pixbuf, buf, 0, j+i, 320);
 			}
 			k = (NOSKIN_WCOLOR + NOSKIN_BCOLOR) - k;
 		}
 	}
 
-	gtk_widget_draw (preview, NULL);
+	gtk_image_set_from_pixbuf (GTK_IMAGE (image), pixbuf);
+
+	g_object_unref (G_OBJECT (pixbuf));
 }
 
 
-void draw_q2_skin (GtkWidget *preview, guchar *data, int scale) {
+void draw_q2_skin (GtkWidget *image, guchar *data, int scale) {
+	GdkPixbuf *pixbuf;
 	guchar *buf;
 	guchar *ptr;
 	int i, j, k;
+
+	pixbuf = gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, 32*scale, 32*scale);
+	pixbuf_draw_set_gamma (1.5);
 
 	buf = g_malloc (32*scale*3);
 
@@ -226,7 +298,7 @@ void draw_q2_skin (GtkWidget *preview, guchar *data, int scale) {
 				data++;
 			}
 			for (k = 0; k < scale; k++) {
-				gtk_preview_draw_row (GTK_PREVIEW (preview), buf, 0, i++, 32*scale);
+				pixbuf_draw_row (pixbuf, buf, 0, i++, 32*scale);
 			}
 		}
 	}
@@ -238,7 +310,7 @@ void draw_q2_skin (GtkWidget *preview, guchar *data, int scale) {
 				i = (NOSKIN_WCOLOR + NOSKIN_BCOLOR) - i;
 			}
 			for (i = 0; i < 32; i++) {
-				gtk_preview_draw_row (GTK_PREVIEW (preview), buf, 0, j+i, 32*scale);
+				pixbuf_draw_row (pixbuf, buf, 0, j+i, 32*scale);
 			}
 			k = (NOSKIN_WCOLOR + NOSKIN_BCOLOR) - k;
 		}
@@ -246,7 +318,9 @@ void draw_q2_skin (GtkWidget *preview, guchar *data, int scale) {
 
 	g_free (buf);
 
-	gtk_widget_draw (preview, NULL);
+	gtk_image_set_from_pixbuf (GTK_IMAGE (image), pixbuf);
+
+	g_object_unref (G_OBJECT (pixbuf));
 }
 
 
