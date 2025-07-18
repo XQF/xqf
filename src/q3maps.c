995d5ccea169c5cecd48cecacde4ba2e6e260306
Thomas Debesse <dev@illwieckz.net>
2025-07-18 18:15:03 +0200

    implement CRN/DDS/KTX levelshot loading
    Since the existing code relies on GdkPixbuf for processing
    the images, the laziest way to do it was to implement
    a CRN/DDS/KTX to TGA conversion with crnlib and feed
    the TGA to GdkPixbuf.

--- src/q3maps.c
+++ src/q3maps.c
@@ -34,6 +34,7 @@
 #include "utils.h"
 
 #include "q3maps.h"
+#include "crntotga.h"
 
 struct q3mapinfo
 {
@@ -156,11 +157,16 @@ static inline const char* last_two_entries(const char* name) {
 }
 
 static gchar* has_known_image_format(const gchar* name) {
-	guint i, ext_num = 3;
-	gchar *ext_list[3] = {
+	guint i, ext_num = 6;
+	gchar *ext_list[6] = {
+		// Native GdkPixbuf loader.
 		".jpg",
 		".png",
 		".tga",
+		// Crunch TGA conversion before GdkPixbuf.
+		".crn",
+		".dds",
+		".ktx",
 	};
 
 	for (i = 0; i < ext_num; i++) {
@@ -665,6 +671,8 @@ static size_t readimagefromzip(guchar** buf, const char* zipfile, const char* fi
 	zf = unzOpen(zipfile);
 	g_return_val_if_fail(zf!=NULL, 0);
 
+	debug(4, "reading %s from %s", filename, zipfile);
+
 	do
 	{
 #ifdef MZ_VERSION // minizip 2
@@ -719,7 +727,37 @@ static size_t readimagefromzip(guchar** buf, const char* zipfile, const char* fi
 	unzClose(zf);
 
 	if (!error)
-		return buflen;
+	{
+		enum crunchFormat_t format = CRUNCH_NONE;
+
+		if (stri_has_ext(filename, ".crn"))
+		{
+			format = CRUNCH_CRN;
+		}
+		else if (stri_has_ext(filename, ".dds"))
+		{
+			format = CRUNCH_DDS;
+		}
+		else if (stri_has_ext(filename, ".ktx"))
+		{
+			format = CRUNCH_KTX;
+		}
+
+		if ( format == CRUNCH_NONE )
+		{
+			return buflen;
+		}
+
+		unsigned int tgabuf_len;
+		guchar *tgabuf = convert_crn_tga(*buf, buflen, format, &tgabuf_len);
+
+		if (tgabuf)
+		{
+			g_free(*buf);
+			*buf = tgabuf;
+			return tgabuf_len;
+		}
+	}
 
 	g_free(*buf);
 	*buf=NULL;
